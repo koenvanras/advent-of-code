635eb0524c40e7e48f785925f0d7065036d176fc
Koen van Ras-Neve <koen@apparare.nl>
2025-11-05 20:04:33 +0100

    Day 1: Cleanup + added comments + removed ex rethrow

--- 2024/DayOne/DayOne/Program.cs
+++ 2024/DayOne/DayOne/Program.cs
@@ -7,16 +7,20 @@
 	List<int> ListTwo = File.ReadLines("./input.txt")
                             .Select(line => int.Parse(line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries)[1]))
                             .OrderBy(number => number)
-                            .ToList(); ;
-
+                            .ToList();
 	int result = 0;
 
+    // Note: Looping list one count because there should always be 2 points to calculate distance
     for (int i = 0; i < ListOne.Count; i++)
 	{
+        // Print calculation information to console
         Console.WriteLine($"{i}: {ListOne[i]} -> {ListTwo[i]} = {Math.Abs(ListOne[i] - ListTwo[i])}");
+
+        // Add calculation between points too the result
         result += Math.Abs(ListOne[i] - ListTwo[i]);
     }
 
+    // Print result to console
     Console.WriteLine(result.ToString());
 
     Console.ReadLine();
@@ -24,5 +28,5 @@
 catch (Exception ex)
 {
 	Console.WriteLine("Exception: " + ex.Message);
-	throw ex;
+	throw;
 }
