a861f43f452bba6892e1b6edd7a04df0dc957a86
Koen van Ras-Neve <koen@apparare.nl>
2025-11-11 15:50:01 +0100

    Day 3: Part 1

---
+++ 2024/DayTwo - Copy/DayThree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string input = System.IO.File.ReadAllText("./input.txt");
+Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+var result = 0;
+
+foreach (Match match in regex.Matches(input))
+{
+    var mul = int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    result += mul;
+}
+
+Console.WriteLine($"{result}");
+
+Console.ReadLine();
