772ba426a1ce3ada319f04f6de9a6a1565aac7cc
Koen van Ras-Neve <koen@apparare.nl>
2025-11-11 16:05:42 +0100

    Day 3: Part 2

--- 2024/DayTwo - Copy/DayThree/Program.cs
+++ 2024/DayTwo - Copy/DayThree/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System.Text.RegularExpressions;
 
-string input = System.IO.File.ReadAllText("./input.txt");
-Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+string input = File.ReadAllText("./input.txt");
+
+// Regex to extract all the text before don't() and between do() and don't()
+Regex doRegex = new Regex(@"^(.*?)(?=don'?t\(\))|do\(\)(.*?)(?=don'?t\(\)|$)", RegexOptions.Singleline);
+// Regex to extract all mul(number, number) where number is 1 to 3 digits
+Regex mulRegex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
 
 var result = 0;
 
-foreach (Match match in regex.Matches(input))
+// Loop over all text based on the "do calculate" regex
+foreach (Match doMatch in doRegex.Matches(input))
 {
-    var mul = int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
-    result += mul;
+    // Get the group of matches before the first don't() (Groups[1]) or between do() and don't() (Groups[2])
+    var matchGroup = doMatch.Groups[1].Success ? doMatch.Groups[1].Value : doMatch.Groups[2].Value;
+
+    foreach (Match mulMatch in mulRegex.Matches(matchGroup))
+    {
+        // Calculate based on mul regex
+        var mul = int.Parse(mulMatch.Groups[1].Value) * int.Parse(mulMatch.Groups[2].Value);
+        result += mul;
+    }
 }
 
+// Print the result
 Console.WriteLine($"{result}");
 
 Console.ReadLine();
